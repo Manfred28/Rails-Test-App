10a6edaf1cb1d0480cf5757b71b675ca98cb0259
István Kozma <pisti_kozma@hotmail.com>
2021-01-17 13:05:19 +0100

    add supplier_code column to hotel

---
+++ db/migrate/20210117120250_add_supplier_code_to_hotel.rb
@@ -0,0 +1,5 @@
+class AddSupplierCodeToHotel < ActiveRecord::Migration[6.0]
+  def change
+    add_column :hotels, :supplier_code, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_112537) do
+ActiveRecord::Schema.define(version: 2021_01_17_120250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_01_17_112537) do
     t.jsonb "booking_conditions"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "supplier_code"
   end
 
   create_table "suppliers", force: :cascade do |t|
