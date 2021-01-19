777d3bbd95247663cd68ea7fc1313913e4b3f45e
István Kozma <pisti_kozma@hotmail.com>
2021-01-19 17:27:36 +0100

    add enum for classification to amenity

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,4 +1,10 @@
 class Amenity < ApplicationRecord
   has_many :hotel_amenities
   has_many :hotels, through: :hotel_amenities
+
+  enum classification: {
+    general: 10,
+    room: 20,
+    other: 30
+  }
 end
