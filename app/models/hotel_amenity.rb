5170bb685a423688ea6a00d91b4fe56829d39993
István Kozma <pisti_kozma@hotmail.com>
2021-01-19 17:28:02 +0100

    add callbacks to hotel amenity to update denorm_amenities

--- app/models/hotel_amenity.rb
+++ app/models/hotel_amenity.rb
@@ -1,4 +1,29 @@
 class HotelAmenity < ApplicationRecord
   belongs_to :hotel
   belongs_to :amenity
+
+  after_create :destroy_duplicate_other_amenity
+  after_save :insert_into_hotel_denorm_amenities
+
+  def insert_into_hotel_denorm_amenities
+    return if hotel.denorm_amenities[amenity.classification]&.include?(amenity.name)
+
+    hotel.denorm_amenities[amenity.classification] ||= []
+    hotel.denorm_amenities[amenity.classification] << amenity.name
+    hotel.update_column(:denorm_amenities, hotel.denorm_amenities)
+  end
+
+
+  def destroy_duplicate_other_amenity
+    return if amenity.classification == 'other' ||
+    !(other_amenity = hotel.amenities.find_by(name: amenity.name, classification: 'other'))
+
+    other_amenity.destroy!
+    remove_from_hotel_denorm_amenities
+  end
+
+  def remove_from_hotel_denorm_amenities
+    hotel.denorm_amenities['other'].delete(amenity.name)
+    hotel.update_column(:denorm_amenities, hotel.denorm_amenities)
+  end
 end
